0751cf4ee9bc7c58f1e7a2bff9157e528b2db42e
Eya-BHS <benhasseneya28@gmail.com>
2025-12-05 10:57:44 +0100

    Add PasswordValidator.java

---
+++ src/main/java/org/example/PasswordValidator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public final class PasswordValidator {
+
+        public static boolean hasMinLength(String password, int min) {
+            // TODO: implement
+        }
+
+        public static boolean containsDigit(String password) {
+            // TODO: implement
+        }
+
+        public static boolean containsUpperAndLower(String password) {
+            // TODO: implement
+        }
+
+        public static boolean isCommonPassword(String password) {
+            // TODO: implement
+        }
+
+        // Bonus:
+        public static boolean containsSpecialChar(String password, String allowed) {
+            // TODO: implement
+        }
+
+        // Optional:
+        public static boolean isValid(String password) {
+            // TODO: call the other methods
+        }
+
+}
